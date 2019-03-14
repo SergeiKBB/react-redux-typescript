bc66817866f4a38a16ada1c72c58c64fdd334a67
Sergei <sergei.x75v@gmail.com>
2019-03-14 13:01:19 +0200

    added filtering case insensitive

--- src/helpers/core.ts
+++ src/helpers/core.ts
@@ -1,7 +1,7 @@
 import {initialUser, User} from "../interfaces/core";
 
 export function filterUsers(users: User[], name: string, city: string) {
-    return users.filter(user => (user.name.includes(name) && user.city.includes(city)))
+    return users.filter(user => ((user.name).toLowerCase().includes(name.toLowerCase()) && (user.city).toLowerCase().includes(city.toLowerCase())))
 }
 
 export function transformUser(user: initialUser) {
